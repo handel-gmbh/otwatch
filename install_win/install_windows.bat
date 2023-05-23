89facc221c79fac868f9a220a26e5e11a90498b9
Jonathan Handel <71558199+jonathanhandel@users.noreply.github.com>
2023-05-23 14:27:35 +0200

    Korrekturen

--- install_win/install_windows.bat
+++ install_win/install_windows.bat
@@ -1,23 +1,14 @@
-
-D:\otx\sw\otwatch\nssm.exe set otwatch_client AppDirectory "D:\otx\sw\otwatch\prometheus_node_exporter"
-D:\otx\sw\otwatch\nssm.exe set otwatch_client AppParameters "--config.file=D:\otx\sw\otwatch\prometheus_node_exporter\config.yml"
-D:\otx\sw\otwatch\nssm.exe set otwatch_client DisplayName otwatch_client
-D:\otx\sw\otwatch\nssm.exe set otwatch_client Description otwatch client node exporter for prometheus
-D:\otx\sw\otwatch\nssm.exe set otwatch_client Start SERVICE_AUTO_START
-net start otwatch_client
-pause
- 
-echo Installation Prometheus
+echo "Installation Prometheus"
 D:\otx\sw\otwatch\nssm.exe install otwatch_prometheus "D:\otx\sw\otwatch\prometheus\prometheus.exe"
 D:\otx\sw\otwatch\nssm.exe set otwatch_prometheus AppDirectory "D:\otx\sw\otwatch\prometheus"
-D:\otx\sw\otwatch\nssm.exe set otwatch_prometheus AppParameters "--config.file=D:\otx\sw\otwatch\prometheus\prometheus.yml --web.enable-admin-api --web.external-url=https://<fqdn>/prometheus --web.route-prefix="/prometheus" --storage.tsdb.retention.time="365d""
+D:\otx\sw\otwatch\nssm.exe set otwatch_prometheus AppParameters "--web.enable-admin-api   --web.external-url=https://otwatch.<domain>/prometheus  --web.route-prefix="/prometheus"  --storage.tsdb.retention.time="30d"  --log.level=debug --web.enable-lifecycle"
 D:\otx\sw\otwatch\nssm.exe set otwatch_prometheus DisplayName otwatch_prometheus
-D:\otx\sw\otwatch\nssm.exe set otwatch_prometheus Description otwatch prometheus Metics Database
+D:\otx\sw\otwatch\nssm.exe set otwatch_prometheus Description otwatch prometheus database
 D:\otx\sw\otwatch\nssm.exe set otwatch_prometheus Start SERVICE_AUTO_START
 net start otwatch_prometheus
 pause
- 
-echo Installation Prometheus Blackbox Exporter
+
+echo "Installation Prometheus Blackbox Exporter"
 D:\otx\sw\otwatch\nssm.exe install otwatch_blackbox "D:\otx\sw\otwatch\prometheus_blackbox_exporter\blackbox_exporter.exe"
 D:\otx\sw\otwatch\nssm.exe set otwatch_blackbox AppDirectory "D:\otx\sw\otwatch\prometheus_blackbox_exporter"
 D:\otx\sw\otwatch\nssm.exe set otwatch_blackbox AppParameters "--config.file=D:\otx\sw\otwatch\prometheus_blackbox_exporter\blackbox.yml"
@@ -26,8 +17,8 @@ D:\otx\sw\otwatch\nssm.exe set otwatch_blackbox Description otwatch blackbox exp
 D:\otx\sw\otwatch\nssm.exe set otwatch_blackbox Start SERVICE_AUTO_START
 net start otwatch_blackbox
 pause
- 
-echo Installation Prometheus Alert Manager
+
+echo "Installation Prometheus Alert Manager"
 D:\otx\sw\otwatch\nssm.exe install otwatch_alertmanager "D:\otx\sw\otwatch\prometheus_alertmanager\alertmanager.exe"
 D:\otx\sw\otwatch\nssm.exe set otwatch_alertmanager AppDirectory "D:\otx\sw\otwatch\prometheus_alertmanager"
 D:\otx\sw\otwatch\nssm.exe set otwatch_alertmanager AppParameters "--config.file=D:\otx\sw\otwatch\prometheus_alertmanager\alertmanager.yml"
@@ -37,11 +28,32 @@ D:\otx\sw\otwatch\nssm.exe set otwatch_alertmanager Start SERVICE_AUTO_START
 net start otwatch_alertmanager
 pause
 
-echo Installation Prometheus Pushgateway 
-D:\otx\sw\otwatch\nssm.exe install otwatch_pushgateway "D:\otx\sw\otwatch\prometheus_alertmanager\alertmanager.exe"
+echo "Installation Prometheus Pushgateway" 
+D:\otx\sw\otwatch\nssm.exe install otwatch_pushgateway "D:\otx\sw\otwatch\prometheus_pushgatewayr\pushgateway.exe"
 D:\otx\sw\otwatch\nssm.exe set otwatch_pushgateway AppDirectory "D:\otx\sw\otwatch\prometheus_pushgateway"
+D:\otx\sw\otwatch\nssm.exe set otwatch_pushgateway AppParameters "--persistence.file="D:\otx\sw\otwatch\prometheus_pushgateway\memory.txt" --persistence.interval=5m  --web.route-prefix="/pushgateway""
 D:\otx\sw\otwatch\nssm.exe set otwatch_pushgateway DisplayName otwatch_pushgateway
 D:\otx\sw\otwatch\nssm.exe set otwatch_pushgateway Description otwatch Pushgateway for prometheus
 D:\otx\sw\otwatch\nssm.exe set otwatch_pushgateway Start SERVICE_AUTO_START
 net start otwatch_pushgateway 
+pause
+
+
+echo "Installation Prometheus Proxy"
+D:\otx\sw\otwatch\nssm.exe install otwatch_proxy "D:\otx\sw\otwatch\nginx\nginx.exe"
+D:\otx\sw\otwatch\nssm.exe set otwatch_proxy AppDirectory "D:\otx\sw\otwatch\nginx"
+D:\otx\sw\otwatch\nssm.exe set otwatch_proxy DisplayName otwatch_proxy
+D:\otx\sw\otwatch\nssm.exe set otwatch_proxy Description otwatch proxy for prometheus
+D:\otx\sw\otwatch\nssm.exe set otwatch_proxy Start SERVICE_AUTO_START
+net start otwatch_proxy
+pause
+
+
+echo "Installation Prometheus SNMP Exporter"
+D:\otx\sw\otwatch\nssm.exe install otwatch_snmp_exporter "D:\otx\sw\otwatch\prometheus_snmp_exporter\snmp_exporter.exe"
+D:\otx\sw\otwatch\nssm.exe set otwatch_snmp_exporter AppDirectory "D:\otx\sw\otwatch\prometheus_snmp_exporter"
+D:\otx\sw\otwatch\nssm.exe set otwatch_snmp_exporter DisplayName otwatch_snmp_exporter
+D:\otx\sw\otwatch\nssm.exe set otwatch_snmp_exporter Description otwatch_snmp_exporter for prometheus
+D:\otx\sw\otwatch\nssm.exe set otwatch_snmp_exporter Start SERVICE_AUTO_START
+net start otwatch_snmp_exporter
 pause
---
+++ proxy/cert/createsslcerts.bat
@@ -0,0 +1,2 @@
+"D:\otx\kit\0523 Install otwatch\openssl-3.1.0\openssl-3\x86\bin\openssl" pkcs12 -in "D:\otx\sw\otwatch\cert\otwatch.pfx" -clcerts -nokeys -out D:\otx\sw\otwatch\cert\otwatch.cer
+"D:\otx\kit\0523 Install otwatch\openssl-3.1.0\openssl-3\x86\bin\openssl" pkcs12 -in "D:\otx\sw\otwatch\cert\otwatch.pfx" -nocerts -nodes  -out D:\otx\sw\otwatch\cert\otwatch.key
